331441c85d5a0955a50fb38be838110400dbc3d0
Adrian Rosén <42138011+adrianrosen@users.noreply.github.com>
2023-06-26 15:28:53 +0200

    Updated with correct hash

--- Formula/flex-cli.rb
+++ Formula/flex-cli.rb
@@ -2,7 +2,7 @@ class FlexCli < Formula
     desc "A command-line tool written in Rust to log flexible working hours."
     homepage "https://github.com/adrianrosen/homebrew-flex-cli"
     url "https://github.com/adrianrosen/homebrew-flex-cli/releases/download/v0.1/flex-cli.tar.gz"
-    sha256 "abc123abc123abc123abc123abc123abc123abc123abc123abc123abc123abc1"
+    sha256 "5de55403fd251bd041930ce5805b4e3bce02db81807907f294e80aa3487ec26a"
     license "MIT"
     version "0.1"
   
